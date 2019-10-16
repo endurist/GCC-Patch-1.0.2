b94d1988f7622e09daab371ffdbf6a18c9688f48
cwang52 <cwang52@ncsu.edu>
2019-10-16 13:34:09 -0400

    <Update the first and second problem>

--- java/src/main/java/skeleton/answers/Question1.java
+++ java/src/main/java/skeleton/answers/Question1.java
@@ -3,8 +3,27 @@ package skeleton.answers;
 public class Question1 {
 
     public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
-        // TODO Auto-generated method stub
-        return -5;
+        double totalPayment = 0;
+        double deposite = initialLevelOfDebt * 0.1;
+        
+        // monthly repayment, this should be a minimum of $50
+        double monthlyRepayment = initialLevelOfDebt * repaymentPercentage / 100.0;
+        // make sure the minimum repayment is 50
+        if (monthlyRepayment < 50)
+        	monthlyRepayment = 50;
+        
+        int numOfMonth = 0;
+        
+        double currentDebt = initialLevelOfDebt;
+        double currentInterest = initialLevelOfDebt * interestPercentage / 100.0;
+        while (currentDebt > 50) {
+        	currentDebt = currentDebt + currentInterest - monthlyRepayment;
+        	currentInterest = currentDebt * interestPercentage / 100.0;
+        	numOfMonth++;
+        }
+        totalPayment = numOfMonth * monthlyRepayment + currentDebt + deposite;
+        
+        return totalPayment;
     }
 
 }
--- java/src/main/java/skeleton/answers/Question2.java
+++ java/src/main/java/skeleton/answers/Question2.java
@@ -3,7 +3,20 @@ package skeleton.answers;
 public class Question2 {
 
     public static int riskAndReward(int[] risk, int[] bonus, int[] trader) {
-        // TODO Auto-generated method stub
-        return -5;
+        // array length
+        int len = risk.length;
+        int overallBonus = 0;
+        int eachBonus = 0;
+        
+        for (int i = 0; i < len; i++) {
+        	for (int j = 0; j < len; j++) {
+        		if (trader[i] >= risk[j] && bonus[j] > eachBonus)
+        			eachBonus = bonus[j];
+        	}
+        	overallBonus += eachBonus;
+        	eachBonus = 0;
+        }
+        
+        return overallBonus;
     }
 }
